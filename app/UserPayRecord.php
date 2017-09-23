31c64fd7db09044f4c479a1488a7ebbdfbfa1765
yanjiadong <452937595@qq.com>
2017-09-23 16:39:51 +0800

    完善功能

--- app/UserPayRecord.php
+++ app/UserPayRecord.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserPayRecord extends Model
 {
-    protected $table = 'user_pay_record';
+    protected $table = 'user_pay_records';
     protected $fillable = ['user_id','type','pay_type','price'];
 }
