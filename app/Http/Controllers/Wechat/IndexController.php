61d793bba7efa20e3f515b86cfc0444dcb910c99
yanjiadong <452937595@qq.com>
2017-09-23 10:42:30 +0800

    完善功能

--- app/Http/Controllers/Wechat/IndexController.php
+++ app/Http/Controllers/Wechat/IndexController.php
@@ -58,14 +58,13 @@ class IndexController extends BaseController
                     //dd($success);
                     session(['user_id'=>$success->id]);
 
-                    return redirect()->route('wechat.index.index');
-
-                    $to_url = url('wechat/index/index');
+                    /*$to_url = url('wechat/index/index');
                     Header("Location: $to_url");
-                    exit();
+                    exit();*/
                 }
-
             }
+
+            return redirect()->route('wechat.index.index');
         }
         else
         {
