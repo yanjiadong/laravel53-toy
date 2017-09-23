04fbba81a81404613597fccd041c2836192238a9
yanjiadong <452937595@qq.com>
2017-09-23 14:45:53 +0800

    完善功能

--- app/VipCardPay.php
+++ app/VipCardPay.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class VipCardPay extends Model
 {
     protected $table = 'vip_card_pays';
-    protected $fillable = ['user_id','vip_card_id','days','price','status','pay_status'];
+    protected $fillable = ['user_id','vip_card_id','days','price','status','pay_status','order_code','money'];
 }
