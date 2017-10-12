80e4ca371d9b6c1cd07182e2f20b1fe976cfec4a
yanjiadong <452937595@qq.com>
2017-10-12 14:17:27 +0800

    完善功能

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -328,7 +328,7 @@ function filterEmoji($str)
     $str = preg_replace_callback(
         '/./u',
         function (array $match) {
-            return strlen($match[0]) >= 4 ? '' : $match[0];
+            return strlen($match[0]) >= 4 ? '?' : $match[0];
         },
         $str);
 
