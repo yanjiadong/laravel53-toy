5296fa8c0218a50b667d08ca7ff297c359613794
yanjiadong <452937595@qq.com>
2017-11-01 17:52:38 +0800

    完善功能

--- app/Http/Controllers/Wechat/ZhimaController.php
+++ app/Http/Controllers/Wechat/ZhimaController.php
@@ -38,7 +38,7 @@ class ZhimaController extends BaseController
         //授权获取芝麻信息
         $result = TestZhimaGetResult($params,$sign);
 
-        $user_id = '3';
+        $user_id = 29;
         //$result = 'open_id=268817768944808448617928815&error_message=%E6%93%8D%E4%BD%9C%E6%88%90%E5%8A%9F&state=%E5%95%86%E6%88%B7%E8%87%AA%E5%AE%9A%E4%B9%89&error_code=SUCCESS&app_id=300000622&success=true';
         Zmxy::create(['user_id'=>$user_id,'info'=>$result]);
 
