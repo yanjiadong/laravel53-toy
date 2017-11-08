2c7f1c466525b1ccdb6aeaf0fd4d546ce534d3ac
yanjiadong <452937595@qq.com>
2017-11-08 16:36:33 +0800

    完善功能

--- resources/views/wechat/user/deposit1.blade.php
+++ resources/views/wechat/user/deposit1.blade.php
@@ -154,6 +154,28 @@
 </script>
 <script>
     $(function () {
+        pushHistory();
+        /*----------避免下一页返回这一页调用这个函数-------------*/
+        var bool=false;
+        setTimeout(function(){
+            bool=true;
+        },1500);
+        window.addEventListener("popstate", function(e) {  //回调函数中实现需要的功能
+            if(bool) {
+                location.href = document.referrer;  //在这里指定其返回的地址  订单列表页面
+            }
+        }, false);
+    });
+    function pushHistory() {
+        var state = {
+            title: "title",
+            url: "#"
+        };
+        window.history.pushState(state, state.title, state.url);
+    }
+</script>
+{{--<script>
+    $(function () {
         $(".toys-car").height($(window).height());
         pushHistory();
         window.addEventListener("popstate", function(e) {  //回调函数中实现需要的功能
@@ -167,6 +189,6 @@
         };
         window.history.pushState(state, state.title, state.url);
     }
-</script>
+</script>--}}
 </body>
 </html>
