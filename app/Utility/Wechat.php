5c5742b7aa162d4b8fb7e8bb56898c177b0d584a
yanjiadong <452937595@qq.com>
2017-11-09 10:13:52 +0800

    test

--- app/Utility/Wechat.php
+++ app/Utility/Wechat.php
@@ -3,11 +3,35 @@ namespace App\Utility;
 
 class Wechat
 {
+    public function __construct()
+    {
+        include_once __DIR__. "/socketlog/slog.function.php";
+
+        //配置
+        slog(array(
+            'host'                => 'localhost',//websocket服务器地址，默认localhost
+            'optimize'            => false,//是否显示利于优化的参数，如果运行时间，消耗内存等，默认为false
+            'show_included_files' => false,//是否显示本次程序运行加载了哪些文件，默认为false
+            'error_handler'       => false,//是否接管程序错误，将程序错误显示在console中，默认为false
+            'force_client_ids'    => array(//日志强制记录到配置的client_id,默认为空,client_id必须在allow_client_ids中
+                'yanjiadong',
+                //'client_02',
+            ),
+            'allow_client_ids'    => array(//限制允许读取日志的client_id，默认为空,表示所有人都可以获得日志。
+                'yanjiadong',
+                //'client_02',
+                //'client_03',
+            ),
+        ),'config');
+
+    }
+
     public function responseMsg()
     {
         //$text = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><FromUserName><![CDATA[FromUser]]></FromUserName><CreateTime>123456789</CreateTime><MsgType><![CDATA[event]]></MsgType><Event><![CDATA[subscribe]]></Event></xml>";
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
+        slog('----'.$postStr.'----');
         if(!empty($postStr))
         {
             libxml_disable_entity_loader(true);
