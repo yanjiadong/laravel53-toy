a6ca9f86c5f9343cde565b87856139ae966fbc03
yanjiadong <452937595@qq.com>
2017-11-09 12:57:43 +0800

    test

--- app/Http/Controllers/Wechat/IndexController.php
+++ app/Http/Controllers/Wechat/IndexController.php
@@ -21,6 +21,7 @@ class IndexController extends BaseController
 {
     public function __construct()
     {
+        parent::__construct();
         /*include_once dirname(dirname(dirname(__DIR__))). "/Utility/socketlog/slog.function.php";
 
         //配置
@@ -527,35 +528,6 @@ class IndexController extends BaseController
         }else{
             return false;
         }*/
-    }
-
-    private function handleEvent($postObj)
-    {
-        $contentStr = '';
-        switch($postObj->Event)
-        {
-            case 'subscribe':
-                $contentStr .= "hi,欢迎关注趣编程,这里有全球最潮流的益智类编程教育玩具，让孩子在玩乐中培养创造与逻辑分析能力，学会未来用编程与世界沟通。每月租金只需299，快来体验吧！";
-                break;
-            case 'unsubscribe':
-                break;
-        }
-
-        $resultStr = $this->responseText($postObj, $contentStr);
-        return $resultStr;
-    }
-
-    private function responseText($postObj, $contentStr)
-    {
-        $textTpl = "<xml>
-                    <ToUserName><![CDATA[%s]]></ToUserName>
-                    <FromUserName><![CDATA[%s]]></FromUserName>
-                    <CreateTime>%s</CreateTime>
-                    <MsgType><![CDATA[text]]></MsgType>
-                    <Content><![CDATA[%s]]></Content>
-                    </xml>";
-        $resultStr = sprintf($textTpl, $postObj->FromUserName, $postObj->ToUserName, time(), $contentStr);
-        return $resultStr;
     }
 
     /**
