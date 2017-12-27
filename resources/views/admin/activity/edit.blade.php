99b830b20134e5fb062622ad86b5a65a8ffb20c4
yanjiadong <452937595@qq.com>
2017-12-27 16:35:57 +0800

    新版

--- resources/views/admin/activity/edit.blade.php
+++ resources/views/admin/activity/edit.blade.php
@@ -34,24 +34,37 @@
                             <div class="span3">活动类型：</div>
                             <div class="span9">
                                 <select name="select" class="validate[required]" id="type">
-                                    <option value="0" {{$activity->type==0?'selected':''}}>--请选择--</option>
                                     <option value="1" {{$activity->type==1?'selected':''}}>运营活动</option>
                                     <option value="2" {{$activity->type==2?'selected':''}}>新手指南</option>
                                 </select>
                             </div>
                         </div>
-                        <div class="row-form clearfix">
+
+                        <div class="row-form clearfix" id="activity_show" style="{{ $activity->type==1?'':'display:none;' }}">
                             <div class="span3">活动图片：</div>
                             <div class="span9">
                                 <input type="file" class="userfile" id="upload" value="上传图片" name="userfile" accept="image/jpeg,image/png,image/gif,image/jpg">
-                                <input type="hidden" id="uploadPath" name="uploadPath" value="{{$activity->picture}}" />
                                 <br/>
                                 <img src="{{$activity->picture}}" style="margin-top: 10px;height: 94px;width:320px;" class="showimg" id="showPic"/>
                                 <img src="/admin/assets/img/loading.gif"  id="loading" style="display:none;margin-top:10px;height: 150px;width:150px;" />
                                 <br/>
+                                <span>建议图片尺寸标准为960x260</span>
+                            </div>
+                        </div>
+
+                        <div class="row-form clearfix" id="help_show" style="{{ $activity->type==2?'':'display:none;' }}">
+                            <div class="span3">活动图片：</div>
+                            <div class="span9">
+                                <input type="file" class="userfile" id="upload2" value="上传图片" name="userfile" accept="image/jpeg,image/png,image/gif,image/jpg">
+                                <br/>
+                                <img src="{{$activity->picture}}" style="margin-top: 10px;height: 94px;width:320px;" class="showimg" id="showPic2"/>
+                                <img src="/admin/assets/img/loading.gif"  id="loading2" style="display:none;margin-top:10px;height: 150px;width:150px;" />
+                                <br/>
                                 <span>建议图片尺寸标准为960x300</span>
                             </div>
                         </div>
+
+                        <input type="hidden" id="uploadPath" name="uploadPath" value="{{$activity->picture}}" />
 
                         <div class="footer tar">
                             <button class="btn" id="submit">保 存</button>
@@ -65,9 +78,42 @@
     <script type="text/javascript" src="/admin/assets/js/jquery.ajaxfileupload.js"></script>
     <script type="text/javascript">
         $(document).ready(function() {
+            $("#type").change(function(){
+                if ($(this).val() == 1) {
+                    $("#activity_show").show();
+                    $("#help_show").hide();
+                } else {
+                    $("#activity_show").hide();
+                    $("#help_show").show();
+                }
+            });
+
+            $("#upload2").uniform();
+            $("#upload2").ajaxfileupload({
+                'action': '{{ url('admin/upload',['size'=>'960,300']) }}',
+                'params': {
+                    '_token': Laravel.csrfToken
+                },
+                'onComplete': function(data) {
+                    if (data.success == 1) {
+                        $('#uploadPath').val(data.path);
+                        $('#showPic2').attr('src', data.url).show();
+                    } else {
+
+                    }
+                    $("#loading2").hide();
+                },
+                'onStart': function() {
+                    $("#loading2").show();
+                    $("#showPic2").hide();
+                },
+                'onCancel': function() {
+                }
+            });
+
             $("#upload").uniform();
             $("#upload").ajaxfileupload({
-                'action': '{{ url('admin/upload',['size'=>'960,300']) }}',
+                'action': '{{ url('admin/upload',['size'=>'960,260']) }}',
                 'params': {
                     '_token': Laravel.csrfToken
                 },
