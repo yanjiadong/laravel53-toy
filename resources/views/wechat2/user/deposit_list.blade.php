aca2564b68bd489417ada1d73ddee4954594c4cc
yanjiadong <452937595@qq.com>
2018-01-02 21:08:06 +0800

    新版

--- resources/views/wechat2/user/deposit_list.blade.php
+++ resources/views/wechat2/user/deposit_list.blade.php
@@ -74,5 +74,27 @@
         deposit_list.init();
     })
 </script>
+<script>
+    $(function () {
+        /*----------避免下一页返回这一页调用这个函数-------------*/
+        var bool=false;
+        setTimeout(function(){
+            pushHistory();
+            bool=true;
+        },500);
+        window.addEventListener("popstate", function(e) {  //回调函数中实现需要的功能
+            if(bool) {
+                location.href=document.referrer;  //在这里指定其返回的地址
+            }
+        }, false);
+    });
+    function pushHistory() {
+        var state = {
+            title: "title",
+            url: ""
+        };
+        window.history.pushState(state, state.title, state.url);
+    }
+</script>
 </body>
 </html>
